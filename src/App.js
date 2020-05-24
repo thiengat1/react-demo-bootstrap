5ba059cf24ab7df447ac1d98b65bb693184b2b87
thienga <thiengat1@gmail.com>
2020-05-24 02:50:03 -0700

    demo bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+          <div className="App">
+            <div>
+            <Button color="primary">primary</Button>{' '}
+            <Button color="secondary">secondary</Button>{' '}
+            <Button color="success">success</Button>{' '}
+            <Button color="info">info</Button>{' '}
+            <Button color="warning">warning</Button>{' '}
+            <Button color="danger">danger</Button>{' '}
+            <Button color="link">link</Button>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
